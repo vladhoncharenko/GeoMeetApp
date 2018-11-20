43b2bd4f47759414b769ced35bcd927d8f8d79c1
vladhoncharenko <vladhoncharenko@gmail.com>
2018-11-20 23:04:43 +0200

    Place picker was added.

--- android/app/src/main/java/com/geomeetapp/MainApplication.java
+++ android/app/src/main/java/com/geomeetapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.geomeetapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNGooglePlacePickerPackage;
 import io.radar.react.RNRadarPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.avishayil.rnrestart.ReactNativeRestartPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGooglePlacePickerPackage(),
           new RNRadarPackage(),
           new VectorIconsPackage(),
           new ReactNativeRestartPackage(),
--- src/Components/CreateEvent.js
+++ src/Components/CreateEvent.js
@@ -1,12 +1,12 @@
 
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight, Button, ScrollView } from 'react-native';
 import { StackActions, NavigationActions } from 'react-navigation';
-var t = require('tcomb-form-native');
 import moment from 'moment';
+import RNGooglePlacePicker from 'react-native-google-place-picker';
 
+var t = require('tcomb-form-native');
 var Form = t.form.Form;
-
 
 var Event = t.struct({
     name: t.String,
@@ -14,7 +14,7 @@ var Event = t.struct({
     startTime: t.Date,
     endDate: t.Date,
     endTime: t.Date,
-    radius: t.Number,
+    radius: t.refinement(t.Number, function (s) { return s > 50 && s < 151; }),
     description: t.String
 });
 
@@ -26,7 +26,7 @@ var options = {
         },
         radius: {
             label: 'Радіус',
-            maxLength: 2
+            maxLength: 3
         },
         description: {
             label: 'Опис',
@@ -63,55 +63,93 @@ var options = {
     }
 };
 
-
-
 class CreateEvent extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            location: null,
+            buttonState: true,
+            value: {}
+        }
+    };
 
-    }
-
-
-    login() {
-        const formValues = this.refs.form.getValue();
-        console.log('FORM VALUES', formValues);
+    createLocation() {
         this.props.navigation.dispatch(StackActions.reset({
             index: 0,
             actions: [
                 NavigationActions.navigate({ routeName: 'Events', params: { shouldUpdate: true } })
             ],
         }))
-    }
+    };
 
+    onFormChange(value) {
+        this.setState({ value });
+        const formValues = this.refs.form.getValue();
+        if (formValues && this.state.location) {
+            this.setState({ buttonState: false });
+        } else {
+            this.setState({ buttonState: true });
+        }
+    };
+
+    getLocation() {
+        this.form = this.refs.form;
+        RNGooglePlacePicker.show((response) => {
+            if (response.didCancel) {
+                console.log('User cancelled GooglePlacePicker');
+            }
+            else if (response.error) {
+                console.log('GooglePlacePicker Error: ', response.error);
+            }
+            else {
+                const formValues = this.refs.form.getValue();
+                this.setState({ location: response, buttonState: !formValues });
+            }
+        })
+    };
 
     render() {
         return (
+            <View>
+                <ScrollView>
+                    <Text>Створити новий захід:</Text>
+                    <Form
+                        ref="form"
+                        type={Event}
+                        options={options}
+                        value={this.state.value}
+                        onChange={this.onFormChange.bind(this)}
+                    />
 
-            <View style={styles.container}>
-            <Text>Створити новий захід:</Text>
-                <Form
-                    ref="form"
-                    type={Event}
-                    options={options}
-                />
-                <TouchableHighlight style={styles.button} onPress={() => this.login()} underlayColor='#99d9f4'>
-                    <Text style={styles.buttonText}>Save</Text>
-                </TouchableHighlight>
+                    <TouchableHighlight style={styles.button} onPress={this.getLocation.bind(this)} underlayColor='#99d9f4'>
+                        <Text style={styles.buttonText}>Вибрати локацію</Text>
+                    </TouchableHighlight>
+
+                    <View style={styles.location}>
+                        <Text style={{ color: 'black', fontSize: 15 }}>
+                            {!this.state.location ? ("") : (this.state.location.address)}
+                        </Text>
+                    </View>
+                    <Button title="Створити" style={styles.button} onPress={() => this.createLocation()} disabled={this.state.buttonState} >
+                    </Button>
+
+                </ScrollView>
             </View>
-
-
-
-
         );
     }
 }
 
 var styles = StyleSheet.create({
     container: {
+        flex: 1,
+        flexGrow: 1,
         justifyContent: 'center',
-        marginTop: 10,
-        padding: 20,
-        
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    location: {
+        backgroundColor: 'white',
+        margin: 25
     },
     buttonText: {
         fontSize: 18,
